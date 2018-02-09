39f7d0fe87645eec79a74778dd2d87f3d71a4742
lupones <lupones@inf.upv.es>
2018-02-09 11:40:20 +0100

    Update of 1710 scripts to support new tables

--- scripts/1710-interval-total-tables.py
+++ scripts/1710-interval-total-tables.py
@@ -13,7 +13,7 @@ def main():
     parser.add_argument('-w', '--workloads', required=True, help='.yaml file where the list of workloads is found.')
     parser.add_argument('-od', '--outputdir', default='./output', help='Directory where output files will be placed')
     parser.add_argument('-id', '--inputdir', default='./data', help='Directory where input are found')
-    parser.add_argument('-lw', '--listways', default=[], action='append', help='List of tumples with (number of ways given to crancbmk, nmber of ways given to the rest of apps). Example: (12,8)')
+    parser.add_argument('-p', '--policies', action='append', default=[], help='Policies we want to show results of.         Options: np,hg.')
     args = parser.parse_args()
     Z = 1.96  # value of z at a 95% c.i.
     NUM_REPS_APP = 3  # num of values used to calculate the mean value
@@ -23,10 +23,10 @@ def main():
     numApps = 8
 
     ## EVENTS USED ##
-    ev0 = "MEM_LOAD_UOPS_RETIRED.L3_HIT"
-    ev1 = "MEM_LOAD_UOPS_RETIRED.L3_MISS"
-    ev2 = "CYCLE_ACTIVITY.STALLS_TOTAL"
-    ev3 = "CYCLE_ACTIVITY.STALLS_LDM_PENDING"
+    ev0 = "mem_load_uops_retired.l3_hit"
+    ev1 = "mem_load_uops_retired.l3_miss"
+    ev2 = "cycle_activity.stalls_ldm_pending"
+    ev3 = "intel_cqm/llc_occupancy/"
 
 
     with open(args.workloads, 'r') as f:
@@ -37,38 +37,36 @@ def main():
 
     for wl_id, wl in enumerate(workloads):
 
-            list_ways = args.listways
-
             wl_show_name = "-".join(wl)
+            print(wl_show_name)
+
+            list_ways = args.policies
 
             #create total dataframe
-            columns = ['configuration', 'IPC:mean', 'IPC:ci', 'IPC_prediction:mean', 'median_rel_err_IPC', 'MPKIL3:mean', 'MPKIL3:ci','STP:mean', 'STP:ci', 'ANTT:mean', 'ANTT:ci', 'Unfairness:mean', 'Unfairness:ci', 'Tt:mean', 'Tt:ci']
+            columns = ['policy', 'IPC:mean', 'IPC:ci', 'MPKIL3:mean', 'MPKIL3:ci','STP:mean', 'STP:ci', 'ANTT:mean', 'ANTT:ci', 'Unfairness:mean', 'Unfairness:ci', 'Tt:mean', 'Tt:ci']
             index = range(0, len(list_ways))
             dfTotal = pd.DataFrame(columns=columns, index = index)
 
             numConfig = 0
-            for way_id, way in enumerate(list_ways):
-                aux = way.split('(')
-                aux = aux[1].split(')')
-                aux = aux[0].split(',')
-                waysCR = aux[0]
-                waysO = aux[1]
+            for policy in args.policies:
 
+                outputPathPolicy = outputPath + "/" + policy + "/resultTables"
+                os.makedirs(os.path.abspath(outputPathPolicy), exist_ok=True)
 
                 # total values of execution
-                dfTotal.ix[numConfig,'configuration'] = str(waysCR) + "cr" + str(waysO) + "others"
+                dfTotal.ix[numConfig,'policy'] = policy
 
-                wl_in_path = args.inputdir + "/" + str(waysCR) + "cr" + str(waysO) + "others/outputFilesMean/" + wl_show_name + "-fin.csv"
+                wl_in_path = args.inputdir + "/" + policy + "/data-agg/" + wl_show_name + "_fin.csv"
 
                 print(wl_in_path)
 
                 dfaux = pd.read_table(wl_in_path, sep=",")
 
-                dfaux = dfaux[["instructions:mean","instructions:std","ipnc:mean","ipnc:std",ev1+":mean",ev1+":std"]]
+                dfaux = dfaux[["instructions:mean","instructions:std","ipc:mean","ipc:std",ev1+":mean",ev1+":std"]]
 
 
-                dfTotal.ix[numConfig,'IPC:mean'] = dfaux["ipnc:mean"].sum()
-                dfTotal.ix[numConfig,'IPC:ci'] = Z * ((np.sqrt((dfaux["ipnc:std"]*dfaux["ipnc:std"]).sum())) / SQRT_NUM_REPS_APP)
+                dfTotal.ix[numConfig,'IPC:mean'] = dfaux["ipc:mean"].sum()
+                dfTotal.ix[numConfig,'IPC:ci'] = Z * ((np.sqrt((dfaux["ipc:std"]*dfaux["ipc:std"]).sum())) / SQRT_NUM_REPS_APP)
 
                 #print("dfTotal with IPC")
                 #print(dfTotal)
@@ -87,25 +85,25 @@ def main():
                 #print(dfTotal)
 
                 # dataframe for interval table
-                wl_in_path = args.inputdir + "/" + str(waysCR) + "cr" + str(waysO) + "others/outputFilesMean/" + wl_show_name + ".csv"
+                wl_in_path = args.inputdir + "/" + policy + "/data-agg/" + wl_show_name + ".csv"
                 print(wl_in_path)
 
                 dfWay = pd.read_table(wl_in_path, sep=",")
-                dfWay = dfWay[["interval","app","instructions:mean","instructions:std","ipnc:mean","ipnc:std","ipnc_prediction:mean",ev1+":mean",ev1+":std","l3_kbytes_occ:mean","l3_kbytes_occ:std"]]
+                dfWay = dfWay[["interval","app","instructions:mean","instructions:std","ipc:mean","ipc:std",ev1+":mean",ev1+":std",ev3+":mean",ev3+":std"]]
                 #dfWay = dfWay.set_index(['interval'])
                 #groups = dfWay.groupby(level=[0])
 
                 # calculate confidence interval of IPC
-                dfWay["ipnc:std"] =  Z * ( dfWay["ipnc:std"] / SQRT_NUM_REPS_APP )
+                dfWay["ipc:std"] =  Z * ( dfWay["ipc:std"] / SQRT_NUM_REPS_APP )
 
                 # calculate l3_kbytes_occ in mbytes
-                dfWay["l3_kbytes_occ:mean"] = dfWay["l3_kbytes_occ:mean"] / 1024
-                dfWay["l3_kbytes_occ:std"] = dfWay["l3_kbytes_occ:std"] / 1024
+                dfWay[ev3+":mean"] = dfWay[ev3+":mean"] / 1024
+                dfWay[ev3+":std"] = dfWay[ev3+":std"] / 1024
                 # calculate confidence interval of l3_kbytes_occ
-                dfWay["l3_kbytes_occ:std"] = Z * ( dfWay["l3_kbytes_occ:std"] / SQRT_NUM_REPS_APP )
+                dfWay[ev3+":std"] = Z * ( dfWay[ev3+":std"] / SQRT_NUM_REPS_APP )
 
                 # rename columns
-                dfWay = dfWay.rename(columns={'ipnc:mean': 'IPC:mean', 'ipnc:std': 'IPC:ci', 'ipnc_prediction:mean' : 'IPC_prediction:mean', 'l3_kbytes_occ:mean': 'l3_Mbytes_occ:mean', 'l3_kbytes_occ:std': 'l3_Mbytes_occ:ci'})
+                dfWay = dfWay.rename(columns={'ipc:mean': 'IPC:mean', 'ipc:std': 'IPC:ci', ev3+':mean': 'l3_Mbytes_occ:mean', ev3+':std': 'l3_Mbytes_occ:ci'})
 
                 # calculate MPKI
                 relErrIns = (dfWay["instructions:std"] / dfWay["instructions:mean"] )**2
@@ -118,22 +116,16 @@ def main():
                 dfWay = dfWay.rename(columns={ev1+':mean': 'MPKIL3:mean', ev1+':std': 'MPKIL3:ci'})
 
                 # GENERATE Interval tables app per workload
-                dfWay = dfWay[["interval","app","IPC:mean","IPC:ci","IPC_prediction:mean","l3_Mbytes_occ:mean","l3_Mbytes_occ:ci","MPKIL3:mean","MPKIL3:ci"]]
+                dfWay = dfWay[["interval","app","IPC:mean","IPC:ci","l3_Mbytes_occ:mean","l3_Mbytes_occ:ci","MPKIL3:mean","MPKIL3:ci"]]
 
                 dfWayAux = dfWay
                 dfWay = dfWay.set_index(['interval','app'])
                 groups = dfWay.groupby(level=[1])
 
-                # calculate relative error
-
-                dfWay["rel_err_IPC"] = abs(dfWay["IPC:mean"] - dfWay["IPC_prediction:mean"]) / dfWay["IPC:mean"]
-
-                outputPathWays = os.path.abspath(outputPath+"/"+ str(waysCR) + "cr" + str(waysO) + "others")
-
-                os.makedirs(os.path.abspath(outputPathWays), exist_ok=True)
-
                 # create DF for LLC of all apps in workload
                 columns = ['interval']
+                #print(numApps)
+                #print(dfWay)
                 numRows = dfWay['IPC:mean'].count()/numApps
                 numRows = np.asscalar(np.int16(numRows))
                 index = range(0, numRows)
@@ -143,10 +135,10 @@ def main():
                 LLCDATAF = dfLLC
 
                 for appName, df in groups:
-                    print(appName)
+                    #print(appName)
 
                     #generate interval_data_table
-                    outputPathWaysWorkload = outputPathWays + "/" + wl_show_name
+                    outputPathWaysWorkload = outputPathPolicy + "/" + wl_show_name
                     os.makedirs(os.path.abspath(outputPathWaysWorkload), exist_ok=True)
                     outputPathApp = outputPathWaysWorkload + "/" + appName + "-intervalDataTable.csv"
                     df.to_csv(outputPathApp, sep=',')
@@ -154,6 +146,7 @@ def main():
                     #add column to LLC_occup dataframe
                     #nameC = "LLC" + app
                     #print(list(dfWayAux.columns.values))
+                    #print(numRows)
                     AuxAPP = dfWayAux[dfWayAux.app.isin([appName])]
                     AuxAPP = AuxAPP['l3_Mbytes_occ:mean']
                     AuxAPP.index = range(0, numRows)
@@ -168,7 +161,7 @@ def main():
 
 
                 groups = dfWay.groupby(level=[0])
-                columns = ['interval','IPC:mean','IPC_prediction:mean','rel_err_IPC','IPC:ci','MPKIL3:mean','MPKIL3:ci']
+                columns = ['interval','IPC:mean','IPC:ci','MPKIL3:mean','MPKIL3:ci']
                 numRows = dfWay['IPC:mean'].count()/numApps
                 numRows = np.asscalar(np.int16(numRows))
                 index = range(0, numRows + 1)
@@ -181,18 +174,12 @@ def main():
                     dfTotWays.ix[interval, 'IPC:ci'] = np.sqrt((df['IPC:ci']**2).sum())
                     dfTotWays.ix[interval, 'MPKIL3:mean'] = df['MPKIL3:mean'].sum()
                     dfTotWays.ix[interval, 'MPKIL3:ci'] = np.sqrt((df['MPKIL3:ci']**2).sum())
-                    dfTotWays.ix[interval, 'IPC_prediction:mean'] = df['IPC_prediction:mean'].sum()
 
-                dfTotWays['rel_err_IPC'] = abs(dfTotWays['IPC:mean'] - dfTotWays['IPC_prediction:mean']) / dfTotWays['IPC:mean']
-                outputPathTotWays = outputPathWays + "/" + wl_show_name + "-total-table.csv"
+                outputPathTotWays = outputPathPolicy + "/" + wl_show_name + "-total-table.csv"
                 dfTotWays.to_csv(outputPathTotWays, sep=',')
 
-
-                dfTotal.ix[numConfig,'IPC_prediction:mean'] = dfTotWays['IPC_prediction:mean'].mean()
-                dfTotal.ix[numConfig,'median_rel_err_IPC'] = dfTotWays['rel_err_IPC'].median()
-
                 # df with individual values to calculate ANTT and STP
-                wl_in_path = args.inputdir + "/" + str(waysCR) + "cr" + str(waysO) + "others/outputFilesMean/" + wl_show_name + "-fin.csv"
+                wl_in_path = args.inputdir + "/" + policy + "/data-agg/" + wl_show_name + "_fin.csv"
 
                 dfway = pd.read_table(wl_in_path, sep=",")
                 dfway = dfway.set_index(['app'])
@@ -213,18 +200,23 @@ def main():
                 # and add all the values
                 for app, df in groups:
                     words = app.split('_')
-                    appName = words[1]
+                    if len(words) == 3:
+                        appName = words[1] + "_" + words[2]
+                    else:
+                        appName = words[1]
+                    print(appName)
 
-                    wl_indiv = "/home/lupones/manager/experiments/npIndivInterval/outputFilesMean/" + appName + "-fin.csv"
+                    #wl_indiv = "./data-agg/" + appName + "_fin.csv"
+                    wl_indiv = "/home/lupones/manager/experiments/individual/20w/data-agg/" + appName + "_fin.csv"
                     dfIndiv = pd.read_table(wl_indiv, sep=",")
-                    progressApp = df.ix[0,'ipnc:mean'] / dfIndiv.ix[0,'ipnc:mean']
-                    slowdownApp = dfIndiv.ix[0,'ipnc:mean'] / df.ix[0,'ipnc:mean']
+                    progressApp = df.ix[0,'ipc:mean'] / dfIndiv.ix[0,'ipc:mean']
+                    slowdownApp = dfIndiv.ix[0,'ipc:mean'] / df.ix[0,'ipc:mean']
 
                     progress = progress + progressApp
                     slowdown = slowdown + slowdownApp
 
-                    relErrComp = (df.ix[0,'ipnc:std'] / df.ix[0,'ipnc:mean'])**2
-                    relErrIndiv = (dfIndiv.ix[0,'ipnc:std'] / dfIndiv.ix[0,'ipnc:mean'])**2
+                    relErrComp = (df.ix[0,'ipc:std'] / df.ix[0,'ipc:mean'])**2
+                    relErrIndiv = (dfIndiv.ix[0,'ipc:std'] / dfIndiv.ix[0,'ipc:mean'])**2
                     relERR = np.sqrt(relErrComp + relErrIndiv)
 
                     progressERR = progressERR + (progressApp*relERR)
@@ -247,7 +239,7 @@ def main():
                 dfTotal.ix[numConfig,'ANTT:ci'] = Z * (unfairness / 24)
 
                 # calculate the Workload Turn Around Time
-                path_total = args.inputdir + "/" + str(waysCR) + "cr" + str(waysO) + "others/outputFilesMean/" + wl_show_name + "-tot.csv"
+                path_total = args.inputdir + "/" + policy + "/data-agg/" + wl_show_name + "_tot.csv"
                 dft = pd.read_table(path_total, sep=",")
 
                 WTt_value = dft["interval:mean"].iloc[-1]
@@ -259,11 +251,14 @@ def main():
                 numConfig = numConfig + 1;
 
             #generate dfTotal table
-            outputPathTotal = outputPath + "/" + wl_show_name + "-totalDataTable.csv"
-            dfTotal = dfTotal.set_index(['configuration'])
-            print(outputPathTotal)
+            outputPathTotal = outputPath + "/totalTables"
+            os.makedirs(os.path.abspath(outputPathTotal), exist_ok=True)
+
+            tt = outputPathTotal + "/" + wl_show_name + "-totalDataTable.csv"
+            dfTotal = dfTotal.set_index(['policy'])
+            print(tt)
             print(dfTotal)
-            dfTotal.to_csv(outputPathTotal, sep=',')
+            dfTotal.to_csv(tt, sep=',')
 
 
 # El main es crida des d'ací
--- scripts/1710-interval-total-tables.sh
+++ scripts/1710-interval-total-tables.sh
@@ -5,11 +5,7 @@
 # E.g 170719
 
 workloads=/home/lupones/manager/experiments/$1/workloads$1.yaml
-inputdir=/home/lupones/manager/experiments/$1/
-outputdir=/home/lupones/manager/experiments/outputCSVfiles/$1/
+inputdir=/home/lupones/manager/experiments/$1
+outputdir=/home/lupones/manager/experiments/$1
 
-for i in 0,0 20,20; do
-    listways=$listways" -lw ("$i") "
-done
-
-sudo python3 ./1710-interval-total-tables.py -w $workloads -id $inputdir -od $outputdir $listways
+sudo python3 ./1710-interval-total-tables.py -w $workloads -id $inputdir -od $outputdir -p criticalAloneMEAN -p linux
