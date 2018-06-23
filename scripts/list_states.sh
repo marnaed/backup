535e42b56bf8c8e172bd5bd24a518c9f263c8e02
lupones <lupones@inf.upv.es>
2018-06-23 15:59:18 +0200

    Improvement of script scripts/list_states.sh

--- scripts/list_states.sh
+++ scripts/list_states.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
 # $1 = experiment (e.g. 170919)
-# $2 = policy name (e.g. criticalAware)
 
-inputdir=/home/lupones/manager/experiments/$1/$2/log
-outputdir=/home/lupones/manager/experiments/$1/$2/states/
-
+inputdir=/home/lupones/manager/experiments/$1/criticalAware/log
+outputdir=/home/lupones/manager/experiments/$1/criticalAware/states/
 
 mkdir $outputdir
 
 for filename in $inputdir/*.log; do
-    
-    echo $filename
   
-    touch aux.dat
-    cat $filename | grep -E 'Current state' > aux.dat
+	# list of states from log file
+	listStates=$(cat $filename | grep -E 'Current state')
 
+	# create new file
     fn=$(echo $filename | awk -F'/' '{print $9}')
 	fn=${fn::-4}
     fn=$(echo $outputdir$fn".states")
-    
-
-    echo $fn
+	echo $fn
     touch $fn
 
-    while read l; do
-        echo $l | tail -c 3 >> $fn
-    done <aux.dat
-
-    rm aux.dat
-
-
+	while IFS= read -r line
+	do
+   		echo "$line" | tail -c 3 >> $fn 
+	done < <(printf '%s\n' "$listStates")
 done 
 
 
