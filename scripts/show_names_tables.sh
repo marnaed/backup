3287381fd00653aa7d1f1863b0b63b9a30180df4
lupones <lupones@inf.upv.es>
2018-06-25 09:49:15 +0200

    UPDATE: Corrected root of files

--- scripts/show_names_tables.sh
+++ scripts/show_names_tables.sh
@@ -2,10 +2,10 @@
 # set -x
 
 
-workloads=/home/lupones/manager/experiments/EUROPAR_Results/180215-XPL3/workloads180215-XPL3.yaml
-inputdir=/home/lupones/manager/experiments/EUROPAR_Results/180215-XPL3
-outputdir=/home/lupones/manager/experiments/TFG/aaa
+workloads=/home/lupones/manager/experiments/$1/workloads$1.yaml
+inputdir=/home/lupones/manager/experiments/$1
+outputdir=/home/lupones/manager/experiments/$1
 
-sudo python3 ./show_names_tables.py -w $workloads -id $inputdir -od $outputdir -n power/energy-pkg/ -n power/energy-ram/ -n interval -n unfairness
+sudo python3 ./show_names_tables.py -w $workloads -id $inputdir -od $outputdir -n power/energy-pkg/ -n power/energy-ram/ -n interval -n unfairness -n ipc
 
 
