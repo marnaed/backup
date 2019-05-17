4cc567e751dde71bb426af0e2d9b819681691d2e
lupones <lupones@inf.upv.es>
2019-05-17 12:16:58 +0200

    Script to generate scatter plots of MPKIL3 vs. HPKIL3, where each point is coloured according to its value of IPC. Each plot contains all of the values of each type of application (critical, noncritical, medium, problematic)

---
+++ scripts/scatter-appType-graph.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# set -x
+
+# $1 = experiment 
+# E.g 170719
+
+inputdir=/home/lupones/manager/experiments/individual/2w/resultTables/
+outputdir=/home/lupones/manager/experiments/individual/2w/scatter-2w/
+sudo python3 ./scatter-appType-graph.py -fn critical.yaml -fn noncritical.yaml -fn medium.yaml -fn problematic.yaml -id $inputdir -od $outputdir 
+
+#inputdir=/home/lupones/manager/experiments/190510/test/resultTables/
+#outputdir=/home/lupones/manager/experiments/190510/test/scatter/
+#sudo python3 ./scatter-graph.py -fn /home/lupones/manager/experiments/190510/w.yaml -id $inputdir -od $outputdir 
